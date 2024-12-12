b3ce5b25f78314e0f8726689cdf1fec4053fee24
NickMendel <nick.mendel@web.de>
2024-12-12 12:19:31 +0100

    init project setup

---
+++ src/main/java/io/rapidtogo/rapidtogo_cart/RapidtogoCartApplication.java
@@ -0,0 +1,13 @@
+package io.rapidtogo.rapidtogo_cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapidtogoCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapidtogoCartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/rapidtogo/rapidtogo_cart/RapidtogoCartApplicationTests.java
@@ -0,0 +1,13 @@
+package io.rapidtogo.rapidtogo_cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapidtogoCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
